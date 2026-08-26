6108c19b44ac9d60a0590b9b6fbc5e32f572e6a7
TEBL <teblunde@gmail.com>
2026-08-27 01:43:25 +0200

    ...

--- editor/test/test_utilities.py
+++ editor/test/test_utilities.py
@@ -2,7 +2,6 @@ import unittest
 from lib import utilities
 
 class TestUtilities(unittest.TestCase):
-
     def test_format_padded_id(self):
         self.assertEqual(utilities.format_padded_id(1, width=1), '1')
         self.assertEqual(utilities.format_padded_id(1, width=2), '01')
@@ -15,3 +14,10 @@ class TestUtilities(unittest.TestCase):
     def test_suggest_document_directory(self):
         self.assertEqual(utilities.suggest_document_directory(1), '0001')
         self.assertEqual(utilities.suggest_document_directory(1, 'Test'), '0001-Test')
+        self.assertEqual(utilities.suggest_document_directory(1, 'Test?#"!%&/& '), '0001-Test')
+
+    def test_suggest_asset_path(self):
+        self.assertEqual(utilities.suggest_asset_path(0, '.scr'), '00.scr')
+        self.assertEqual(utilities.suggest_asset_path(0, '.scr', 'test'), '00-test.scr')
+        self.assertEqual(utilities.suggest_asset_path(0, '.scr', 'test.scr'), '00-test.scr')
+        self.assertEqual(utilities.suggest_asset_path(0, '.scr', 'Test?#"!%&/& '), '00-Test.scr')
