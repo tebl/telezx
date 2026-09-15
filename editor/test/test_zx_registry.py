e361621112dca335399144fa0e8184542d71e621
TEBL <teblunde@gmail.com>
2026-09-16 01:41:11 +0200

    ...

---
+++ editor/test/test_zx_registry.py
@@ -0,0 +1,146 @@
+import unittest, tempfile, string, random
+from pathlib import Path
+from lib import utilities, ZXRegistry, ZXRegistryEntry
+
+class TestZXDocument(unittest.TestCase):
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory(prefix='telezx-registry-')
+        self.registry_path = Path(self.temp_dir.name) / f'telezx{ZXRegistry.FILE_EXTENSION}'
+        self.registry = ZXRegistry.create_file(document_path=self.registry_path)
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
+
+    def debug_contents(self):
+        with open(self.registry_path, 'r') as file:
+            print(file.read())
+
+    def test_sync_record(self):
+        self.registry.sync_record(0x1000, 'This is a test', 'Test', ['test_tag', 'test_tag2'])
+        record = self.registry.lookup(0x1000)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.description, 'This is a test')
+        self.assertEqual(self.registry.lookup_abbreviation(0x1000), 'Test')
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag'))
+        self.assertTrue(self.registry.save())
+
+    def test_sync_tag(self):
+        self.registry.sync_tag('test_tag', 'This is a test', 0x9999)
+        tag = self.registry.lookup_tag('test_tag')
+        self.assertIsNotNone(tag)
+        self.assertEqual(tag.title, 'This is a test')
+        self.assertEqual(tag.export_id, 0x9999)
+        self.assertTrue(self.registry.save())
+
+    def test_set_ignored(self):
+        self.registry.sync_record(0x1000, 'This is a test', 'Test', ['test_tag', 'test_tag2'])
+        self.assertIsNotNone(self.registry.lookup(0x1000))
+        self.registry.set_ignored(0x1000, True)
+        self.assertTrue(0x1000 in self.registry.ignored)
+        self.assertIsNone(self.registry.lookup(0x1000))
+
+        self.registry.set_ignored(0x1000, False)
+        self.assertTrue(0x1000 not in self.registry.ignored)
+
+        self.registry.sync_record(0x1000, 'This is a test', 'Test', ['test_tag', 'test_tag2'])
+        record = self.registry.lookup(0x1000)
+        self.assertIsNotNone(record)
+
+        self.assertTrue(self.registry.save())
+
+    def test_tag_records(self):
+        # Sync record then ensure partial tag record exists
+        self.registry.sync_record(0x1000, 'This is a test', 'Test', ['test_tag', 'test_tag2'])
+        record = self.registry.lookup(0x1000)
+        self.assertIsNotNone(record)
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag'))
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag2'))
+
+        # Supplement tag information
+        self.registry.sync_tag('test_tag', 'This is a test', 0x9999)
+        tag = self.registry.lookup_tag('test_tag')
+        self.assertIsNotNone(tag)
+        self.assertEqual(tag.title, 'This is a test')
+        self.assertEqual(tag.export_id, 0x9999)
+
+        # Ensure that the record got added to the tag
+        self.assertTrue(record in tag.entries)
+
+        self.assertTrue(self.registry.save())
+
+    def test_tag_membership_changes(self):
+        self.registry.sync_record(0x1000, 'This is a test', 'Test', ['test_tag', 'test_tag2'])
+        record: ZXRegistryEntry = self.registry.lookup(0x1000)
+        self.assertIsNotNone(record)
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag'))
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag2'))
+
+        # Ensure that record is associated with the tag
+        tag = self.registry.lookup_tag('test_tag')
+        self.assertIsNotNone(tag)
+        self.assertTrue(record in tag.entries)
+
+        # Add new tag and ensure that the object reference is the same
+        self.registry.sync_record(0x1000, 'This is a test', 'Test', ['test_tag', 'test_tag2', 'test_tag3'])
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag3'))
+        self.assertEqual(record, self.registry.lookup(0x1000))
+
+        # Ensure that removing a tag removes both reference and the name from
+        # record tag list
+        self.registry.sync_record(0x1000, 'This is a test', 'Test', ['test_tag3'])
+        self.assertEqual(record.tags, ['test_tag3'])
+        self.assertTrue(record not in tag.entries)
+        self.assertTrue(self.registry.save())
+
+    def test_data_persistence(self):
+        self.registry.sync_record(0x1000, 'This is a test', 'Test', ['test_tag', 'test_tag2'])
+        record: ZXRegistryEntry = self.registry.lookup(0x1000)
+        self.assertIsNotNone(record)
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag'))
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag2'))
+        self.assertTrue(self.registry.save())
+
+        self.registry = ZXRegistry.from_file(document_path=self.registry_path, allow_create=False)
+        record: ZXRegistryEntry = self.registry.lookup(0x1000)
+        self.assertIsNotNone(record)
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag'))
+        self.assertIsNotNone(self.registry.lookup_tag('test_tag2'))
+
+        tag = self.registry.lookup_tag('test_tag')
+        self.assertIsNotNone(tag)
+        self.assertTrue(record in tag.entries)
+        self.assertTrue(self.registry.save())
+
+    def test_random_toc(self):
+        allowed_letters = string.ascii_uppercase + '#'
+        characters = string.ascii_lowercase + string.digits
+        for doc_id in range(0x1000, 0x1000 + 100):
+            name = ''.join(random.choices(characters, k=random.randint(4, 10)))
+            self.registry.sync_record(doc_id, name, name[0:4])
+        for letter, entries in self.registry.generate_TOC_AZ().items():
+            self.assertTrue(letter in allowed_letters)
+            for entry_name, entry_link in entries:
+                pass
+
+    def test_toc_generation(self):
+        self.registry.sync_record(0x0004, 'Page 4', 'p4')
+        self.registry.sync_record(0x1000, 'An article', 'aaaa')
+        self.registry.sync_record(0x1001, 'Believe', 'truth')
+        self.registry.sync_record(0x1002, 'Bozo the clown', 'bozo')
+
+        toc = self.registry.generate_TOC_AZ()
+        self.assertEqual(len(toc['A']), 1)
+        self.assertEqual(len(toc['B']), 2)
+        self.assertEqual(len(toc['C']), 0)
+
+    def test_tag_alphabetical(self):
+        self.registry.sync_record(0x0004, 'Page 4', 'p4', tags=['the_sun'])
+        self.registry.sync_record(0x1000, 'An article', 'aaaa', tags=['test_tag'])
+        self.registry.sync_record(0x0002, 'Bzzzz', 'Bzzz', tags=['test_tag'])
+        self.registry.sync_record(0x1001, 'Believe', 'truth', tags=['test_tag'])
+        self.registry.sync_record(0x1002, 'Bozo the clown', 'bozo', tags=['test_tag'])
+
+        toc = self.registry.generate_tag_AZ('test_tag')
+        self.assertEqual(len(toc['B']), 3)
+        self.assertEqual(toc['B'][0], ['Believe', 0x1001])
+        self.assertEqual(toc['B'][1], ['Bozo the clown', 0x1002])
