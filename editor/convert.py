fc8ca6eed5efb0ea9dc0777ee66247347dec45f8
TEBL <teblunde@gmail.com>
2026-09-24 21:53:53 +0200

    Create text and partial yaml structure

--- editor/convert.py
+++ editor/convert.py
@@ -6,18 +6,29 @@ from lib import ZXScreen, ZXDocument, ZXToken, ZXPage, ZXPage_Overlay, ZXPage_To
 from lib.convert import ATRConverter, TKNConverter, ZXTokenConverter
 
 def cmd_53c(args, parser: ArgumentParser):
-    path_out: Path = Path(args.output_file) if args.output_file else None
-    if path_out:
-        if path_out.suffix not in ZXTokenConverter.get_export_suffixes():
-            parser.error(f'Unknown file extension: {path_out.suffix}')
-    else:
-        path_out = Path(args.input_file).with_suffix(f'.{args.output_format}')
-
+    path_out = __get_path_out(args, parser, ZXTokenConverter.get_export_suffixes(include_period=True, include_text=False))
     print(f'Convert {args.input_file} -> {path_out}:')
     converter: ZXTokenConverter = ATRConverter.import_from(args.input_file)
     converter.export_to(path_out)
-
     print('Done.')
+
+def cmd_zx_token(args, parser: ArgumentParser):
+    path_out = __get_path_out(args, parser, ZXTokenConverter.get_export_suffixes(include_period=True))
+    print(f'Convert {args.input_file} -> {path_out}:')
+    converter: ZXTokenConverter = ZXTokenConverter.open(args.input_file)
+    converter.export_to(path_out)
+    print('Done.')
+
+def __get_path_out(args, parser: ArgumentParser, extensions: list[str]) -> Path:
+    path_out: Path = Path(args.output_file) if args.output_file else None
+    if path_out:
+        if path_out.suffix not in extensions:
+            parser.error(f'Invalid file extension: {path_out.suffix}')
+    else:
+        path_out = Path(args.input_file).with_suffix(f'.{args.output_format}')
+    if args.input_file == path_out:
+        parser.error('Input file and output file can\'t be the same')
+    return path_out
 
 def main():
     parser = ArgumentParser()
@@ -28,11 +39,17 @@ def main():
     parser.add_argument('-d', '--debug', action='store_true', help="Enable debug statements")
     subparsers = parser.add_subparsers(required=True, dest='command')
 
-    parser_assets = subparsers.add_parser('53c', help='Manage assets')
-    parser_assets.add_argument('-i', '--input-file', type=utilities.argument_is_file, required=True)
-    parser_assets.add_argument('-o', '--output-file', type=str)
-    parser_assets.add_argument('-f', '--output-format', choices=ZXTokenConverter.get_export_suffixes(strip_period=True), default='zxtoken')
-    parser_assets.set_defaults(function=cmd_53c)
+    parser_atr = subparsers.add_parser('53c', help='ATR (53c)')
+    parser_atr.add_argument('-i', '--input-file', type=utilities.argument_is_file, required=True)
+    parser_atr.add_argument('-o', '--output-file', type=str, help='Specify output path (extension dictates format)')
+    parser_atr.add_argument('-f', '--output-format', choices=ZXTokenConverter.get_export_suffixes(include_period=False, include_text=False), default='zxtoken', help='Specify output format when output-file not used')
+    parser_atr.set_defaults(function=cmd_53c)
+
+    parser_zx_token = subparsers.add_parser('zx_token', help='TeleZX (ZXToken)')
+    parser_zx_token.add_argument('-i', '--input-file', type=utilities.argument_is_file, required=True)
+    parser_zx_token.add_argument('-o', '--output-file', type=str, help='Specify output path (extension dictates format)')
+    parser_zx_token.add_argument('-f', '--output-format', choices=ZXTokenConverter.get_export_suffixes(include_period=False), default='zxtoken', help='Specify output format when output-file not used')
+    parser_zx_token.set_defaults(function=cmd_zx_token)
 
     args = parser.parse_args()
     if args.debug:
--- editor/lib/convert/zx_token_converter.py
+++ editor/lib/convert/zx_token_converter.py
@@ -1,13 +1,20 @@
+import string, yaml
 from pathlib import Path
 from .base_converter import BaseConverter
-from .. import ZXToken, ZXDocument
+from .. import ZXScreen, ZXToken, ZXDocument, ZXScreenIterator, utilities
 
 class ZXTokenConverter(BaseConverter):
+    EXTENSION_TXT  = '.txt'
+    EXTENSION_YAML = '.yaml'
     zx_token: ZXToken
 
     def __init__(self, zx_token: ZXToken):
         super().__init__()
         self.zx_token = zx_token
+
+    @classmethod
+    def open(cls, input_file: Path) -> ZXTokenConverter:
+        return ZXTokenConverter(ZXToken.from_file(input_file))
 
     def save_as(self, output_path: Path) -> bool:
         previous_path = self.zx_token.document_path
@@ -17,9 +24,15 @@ class ZXTokenConverter(BaseConverter):
         return True
 
     def export_to(self, output_path: Path) -> bool:
-        if not output_path.suffix in self.get_export_suffixes():
+        if not output_path.suffix in self.get_export_suffixes(include_period=True):
             raise RuntimeError(f'Unknown suffix - {output_path.suffix}')
         match output_path.suffix:
+            case self.EXTENSION_TXT:
+                self._log_export(output_path.suffix, output_path)
+                self.export_to_txt(output_path)
+            case self.EXTENSION_YAML:
+                self._log_export(output_path.suffix, output_path)
+                self.export_to_yaml(output_path)
             case ZXDocument.EXTENSION_SCR:
                 self._log_export(output_path.suffix, output_path)
                 self.zx_token.export_to_scr(output_path)
@@ -34,16 +47,72 @@ class ZXTokenConverter(BaseConverter):
                 self.zx_token.export_screenshot(output_path)
         return True
 
+    def export_to_txt(self, output_path: Path):
+        non_printable = 0
+        with open(output_path, 'w') as file:
+            for char_x, char_y in ZXScreenIterator(0, 0, False):
+                char_code = self.zx_token.get_character(char_x, char_y)
+                if char_code >= 0 and chr(char_code) in string.printable:
+                    file.write(chr(char_code))
+                else:
+                    non_printable += 1
+                    file.write(' ')
+                if char_x == (ZXScreen.SCREEN_WIDTH_CHARS - 1):
+                    file.write('\n')
+        if non_printable:
+            self.warning(f'{non_printable} non-printable characters ignored')
+
+    def export_to_yaml(self, output_path: Path):
+        non_printable = 0
+
+        lines = []
+        for char_y in range(0, ZXScreen.SCREEN_HEIGHT_CHARS):
+            lines.append([])
+            for char_x in range(0, ZXScreen.SCREEN_WIDTH_CHARS):
+                char_code = self.zx_token.get_character(char_x, char_y)
+                if char_code >= 0 and chr(char_code) in string.printable:
+                    lines[char_y].append(chr(char_code))
+                else:
+                    non_printable += 1
+                    lines[char_y].append(' ')
+        if non_printable:
+            self.warning(f'{non_printable} non-printable characters ignored')
+
+        with open(output_path, 'w') as file:
+            yaml.dump(
+                { 
+                    'TextDocument' : {
+                        'pages': {
+                            'ZXPage_ClearText': {
+                                'text_lines': [
+                                    utilities.QuotedYAML(''.join(lines[char_y])) for char_y in range(0, ZXScreen.SCREEN_HEIGHT_CHARS)
+                                ]
+                            }
+                        }
+                    }
+                }, 
+                file, 
+                indent=4, 
+                default_flow_style=False, 
+                sort_keys=True
+            )
+
     @classmethod
     def get_format_suffix(cls) -> str:
         return ZXToken.FILE_EXTENSION
 
     @classmethod
-    def get_export_suffixes(cls, strip_period=False) -> str:
-        return [ v[1:] if strip_period else v for v in [ZXToken.FILE_EXTENSION,
-                                                        ZXDocument.EXTENSION_TOKEN,
-                                                        '.png', 
-                                                        '.bmp', 
-                                                        '.jpg', 
-                                                        '.tif',
-                                                        '.scr']]
+    def get_export_suffixes(cls, include_period: bool=True, include_graphical: bool=True, include_text: bool=True) -> list[str]:
+        extensions = []
+        if include_graphical:
+            extensions.append(ZXToken.FILE_EXTENSION)
+            extensions.append(ZXDocument.EXTENSION_TOKEN)
+            extensions.append('.png')
+            extensions.append('.bmp')
+            extensions.append('.jpg')
+            extensions.append('.tif')
+            extensions.append('.scr')
+        if include_text:
+            extensions.append('.txt')
+            extensions.append('.yaml')
+        return [ v[1:] if not include_period else v for v in extensions]
